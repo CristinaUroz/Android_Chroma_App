37a102990d8092c56779dda5c04b3bd321b9ac03
kikevite <brianbvf@gmail.com>
2017-11-27 23:58:04 +0100

    Petits arreglos del layout i strings. Versio 3.

--- app/src/main/java/uroz/cristina/chroma_app/ChromaActivity.java
+++ app/src/main/java/uroz/cristina/chroma_app/ChromaActivity.java
@@ -8,6 +8,6 @@ public class ChromaActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chroma);
+        setContentView(R.layout.chroma_activity);
     }
 }

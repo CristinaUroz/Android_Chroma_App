a5abcbc41ade26497add6c8436fc184d7138d610
kikevite <brianbvf@gmail.com>
2017-11-27 23:38:09 +0100

    Petits arreglos del layout i strings.

--- openCVLibrary331/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary331/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: D:\\APUNTS\\Q7\\programacio_de_mobils_android\\Projecte\\Chroma_App\\openCVLibrary331\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: E:\\AndroidStudioProjects\\Android_Chroma_App\\openCVLibrary331\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**

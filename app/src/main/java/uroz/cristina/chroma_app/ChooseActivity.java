5fb2c0aa52cac05ebba58d1cb40a5dde73fbbe6a
Cristina <cristina5696@gmail.com>
2017-11-29 18:55:32 +0100

    carregar imatges

--- app/src/main/java/uroz/cristina/chroma_app/ChooseActivity.java
+++ app/src/main/java/uroz/cristina/chroma_app/ChooseActivity.java
@@ -1,17 +1,23 @@
 package uroz.cristina.chroma_app;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+//TODO: Borrar buttons
+//TODO: posar previus i next a dalt
 
 public class ChooseActivity extends AppCompatActivity {
     // Declaracio de referencies a elements de la pantalla
     private Button btn_add_fore, btn_add_back, btn_del_fore, btn_del_back, btn_next;
     private Button btn1, btn2;
     private EditText text;
+    private int codi_imatge=1;
 
     // Variables globals
     private boolean visibilitatF = true; // Visibilitat del boto + i de la imatge foreground
@@ -27,16 +33,20 @@ public class ChooseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choose_activity);
 
+
         // Obtencio de referencies a elements de la pantalla
-        btn1 = (Button) findViewById(R.id.prova1);
+
+        ImageView fore_ima = (ImageView)findViewById(R.id.ima_fore);
+        ImageView back_ima = (ImageView)findViewById(R.id.ima_back);
+/*        btn1 = (Button) findViewById(R.id.prova1);
         btn2 = (Button) findViewById(R.id.prova2);
         btn_add_fore = (Button) findViewById(R.id.foreground_add);
         btn_add_back = (Button) findViewById(R.id.background_add);
         btn_del_fore = (Button) findViewById(R.id.foreground_delete);
-        btn_del_back = (Button) findViewById(R.id.background_delete);
+        btn_del_back = (Button) findViewById(R.id.background_delete);*/
         btn_next = (Button) findViewById(R.id.next_button_choose);
 
-        text = (EditText) findViewById(R.id.text1);
+       // text = (EditText) findViewById(R.id.text1);
 
         // Proba passar dades entre dues activitats
 
@@ -48,34 +58,34 @@ public class ChooseActivity extends AppCompatActivity {
         }
         ///////////////////////////////////////////
 
-        btn_add_fore.setVisibility(View.VISIBLE);
-        btn_add_back.setVisibility(View.VISIBLE);
+     /*   btn_add_fore.setVisibility(View.VISIBLE);
+        btn_add_back.setVisibility(View.VISIBLE);*/
 
         // Botons de prova, es reemplaçaran per les imatges que es carregaran
-        btn1.setVisibility(View.INVISIBLE);
-        btn2.setVisibility(View.INVISIBLE);
+//        btn1.setVisibility(View.INVISIBLE);
+  //      btn2.setVisibility(View.INVISIBLE);
 
         // Boto next
         // Passar a la seguent activitat
         btn_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                primera_vegada = true;
+     /*           primera_vegada = true;
                 Intent intent = new Intent(ChooseActivity.this, ChromaActivity.class);
                 // Proba passar dades entre dues activitats
                 String nom = text.getText().toString();
                 intent.putExtra(ChromaActivity.KEY_NOM, nom);
                 intent.putExtra(ChromaActivity.KEY_B, primera_vegada);
                 ///////////////////////////////////////////
-                startActivity(intent);
-                finish();
+                startActivity(intent);*/
+             //   finish();
             }
         });
 
         // Boto delete foreground
         //
         // En lloc d'executarse al fer click al boto, s'ha d'executar quan hi ha una imatge carregada
-        btn_del_fore.setOnClickListener(new View.OnClickListener() {
+    /*    btn_del_fore.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 visibilitatF = !visibilitatF;
@@ -92,23 +102,33 @@ public class ChooseActivity extends AppCompatActivity {
                 visibilitatB = !visibilitatB;
                 cambiaVisivilitatBackground(visibilitatB);
             }
-        });
+        });*/
 
         // Boto add foreground
         //
-        btn_add_fore.setOnClickListener(new View.OnClickListener() {
+        fore_ima.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //startActivityForResult(takePicture, 0);//zero can be replaced with any action code
 
+                Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(pickPhoto , 1);//one can be replaced with any action code
+                codi_imatge=1;
             }
         });
 
         // Boto add background
         //
-        btn_add_back.setOnClickListener(new View.OnClickListener() {
+        back_ima.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //startActivityForResult(takePicture, 0);//zero can be replaced with any action code
 
+                Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(pickPhoto , 1);//one can be replaced with any action code
+                codi_imatge=0;
             }
         });
         //////////////////////////////////////////////////////////////////////////////////////
@@ -157,6 +177,35 @@ public class ChooseActivity extends AppCompatActivity {
         } else {
             btn2.setVisibility(View.VISIBLE);
             btn_add_back.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+
+        ImageView imageview = (ImageView)findViewById(R.id.ima_fore);
+
+        if (codi_imatge==1){
+        imageview = (ImageView)findViewById(R.id.ima_fore);}
+        else if (codi_imatge==0){
+        imageview = (ImageView)findViewById(R.id.ima_back);
+        }
+
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch(requestCode) {
+            case 0:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    imageview.setImageURI(selectedImage);
+                }
+
+                break;
+            case 1:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    imageview.setImageURI(selectedImage);
+                }
+                break;
         }
     }
 }
